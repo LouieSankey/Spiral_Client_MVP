30ace616811900cfd117ecb6d3f8a37b6f2cc480
louissankey <lmsankey@ucdavis.edu>
2020-09-28 14:59:43 -0700

    Updates

---
+++ precache-manifest.7cf95582bccad1add75d0b529e92bb49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62228ca091b4073a25e67b303255465",
+    "url": "/Spiral-React/index.html"
+  },
+  {
+    "revision": "ae55ab35b9766c652112",
+    "url": "/Spiral-React/static/css/2.6d424c78.chunk.css"
+  },
+  {
+    "revision": "b54185e28922b61169e2",
+    "url": "/Spiral-React/static/css/main.27cca48f.chunk.css"
+  },
+  {
+    "revision": "ae55ab35b9766c652112",
+    "url": "/Spiral-React/static/js/2.15e13878.chunk.js"
+  },
+  {
+    "revision": "92fed43b0a8dd00b41f7c2beb10e8072",
+    "url": "/Spiral-React/static/js/2.15e13878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54185e28922b61169e2",
+    "url": "/Spiral-React/static/js/main.924e7ef0.chunk.js"
+  },
+  {
+    "revision": "6c1d1b58a2e2480db20d",
+    "url": "/Spiral-React/static/js/runtime-main.f7f6fb4e.js"
+  },
+  {
+    "revision": "28ce1a40f4d50b68e2d20e3a50f11608",
+    "url": "/Spiral-React/static/media/55golden.28ce1a40.png"
+  },
+  {
+    "revision": "a35a83bb2f3bcd8d4fbb59cdddbeb527",
+    "url": "/Spiral-React/static/media/spiral-galexy-transparent.a35a83bb.png"
+  },
+  {
+    "revision": "1c0aa0ecd10defc6d2474812b87fe602",
+    "url": "/Spiral-React/static/media/spiral-icon.1c0aa0ec.png"
+  }
+]);
