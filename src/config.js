59456e0aac300d3939144b143444c8472ab7e1e4
louissankey <lmsankey@ucdavis.edu>
2020-10-20 17:09:26 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://fierce-peak-53733.herokuapp.com/`,
+  API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://boiling-scrubland-00926.herokuapp.com`,
 
 }
