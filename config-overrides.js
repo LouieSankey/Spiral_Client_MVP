755f056efdffff8c5915fc154fdc8274010cece7
louissankey <lmsankey@ucdavis.edu>
2021-03-20 13:19:59 -0700

    changed config-override.js trying to get production to build

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,15 @@
 module.exports = function override(config, env) {
     config.module.rules.push({
         test: /\.worker\.js$/,
-        use: { loader: 'worker-loader' }
-      })
-    return config;
+        loader : 'worker-loader',
+        options: {
+            name      : 'static/[hash].worker.js',
+            publicPath: '/_next/'
+        }
+    })
+
+// Overcome Webpack referencing `window` in chunks
+config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
+
+return config
   }
